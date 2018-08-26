1aedf54046c92b547e5d5ef3b12a78eed8332c96
gusxo3190 <37891426+gusxo3190@users.noreply.github.com>
2018-08-26 20:36:50 +0900

    Update ManagementBoard.vue

--- src/components/ManagementBoard.vue
+++ src/components/ManagementBoard.vue
@@ -1,142 +1,137 @@
 <template>
   <div class="container">
-    <h3 style="margin-top: 20px">게시판 관리</h3>
+    <h3>매뉴 관리</h3>
     <hr>
-    <a href="#" id="createboard" class="btn btn-outline-dark float-right" data-toggle="modal" data-target="#newboard"><b><i class="fas fa-plus"></i></b>게시판 생성</a>
-
-    <table style="width: 100%;" class="table  table-hover table-sm custab">
-      <thead class="thead-light">
-      <tr>
-        <th scope="col" class="no" style="width: 5%;">#</th>
-        <th scope="col" class="title" >게시판명</th>
-        <th scope="col" class="id" style="width: 10%;">등록자</th>
-        <th scope="col" class="date" style="width: 10%;">등록일</th>
-        <th scope="col" class="mod_del" style="width: 15%;">수정 삭제</th>
-
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <th scope="row" class="no">#</th>
-        <td class="title"><a href="#/board">게시판명</a></td>
-        <td class="id"><a href="#">등록자</a></td>
-        <td class="date">등록일</td>
-        <td class="mod_del">
-
-          <router-link v-bind:to="'/write_post'"><button class=" btn-outline-primary btn-sm" href="#/write_post">수정</button></router-link>
-
-          <button class=" btn-outline-danger btn-sm" data-toggle="modal" data-target="#deleteproject">삭제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row" class="no">#</th>
-        <td class="title">게시판명</td>
-        <td class="id">등록자</td>
-        <td class="date">등록일</td>
-        <td class="mod_del">
-          <button class=" btn-outline-primary btn-sm">수정</button>
-          <button class=" btn-outline-danger btn-sm">삭제</button>
-        </td>
-      </tr>
-      </tbody>
-    </table>
-
-    <div class="row align-items-center justify-content-center">
-      <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item"><a class="page-link" href="#"> << </a></li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#"> >> </a></li>
-        </ul>
-      </nav>
-    </div>
-
-    <!--게시판 생성 모달-->
-    <div class="modal fade" id="newboard" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">게시판 생성</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-
-            <div class="input-group mb-3">
-              <div class="input-group-prepend input-group-lg">
-                <span class="input-group-text" id="basic-addon1">게시판 명</span>
-              </div>
-              <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-            </div>
-
-            <div>
-
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">생성</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">취소</button>
+    <div class="row">
+      <div class="col-lg-3" style="padding: 18.2px">
+        <div class="text-center">
+          <div id="" class="btn-group ">
+            <button class="li_btn color"><i class="fas fa-angle-double-up"></i></button>
+            <button class="li_btn color"><i class="fas fa-angle-up"></i></button>
+            <button class="li_btn color"><i class="fas fa-angle-down"></i></button>
+            <button class="li_btn color"><i class="fas fa-angle-down"></i></button>
+            <button class="li_btn"><i class="fas fa-minus"></i><a>삭제</a></button>
+            <button class="li_btn"><i class="fas fa-plus"></i><a>추가</a></button>
           </div>
         </div>
-      </div>
-    </div>
-
-    <!--수정-->
-
-    <!--삭제-->
-    <div class="modal fade" id="deleteproject" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">프로젝트 삭제</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            정말로 프로젝트를 삭제 하시겠습니까?
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger">삭제</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">취소</button>
-          </div>
+        <div >
+          <ul class="list-group list-group-flush" style="margin-right: 10px; margin-left: 10px">
+            <li type="button" class="list-group-item list-group-item-action">Cras justo odio</li>
+            <li type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</li>
+            <li type="button" class="list-group-item list-group-item-action">Morbi leo risus</li>
+            <li type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</li>
+            <li type="button" class="list-group-item list-group-item-action">Vestibulum at eros</li>
+          </ul>
         </div>
       </div>
-    </div>
+      <div id="div_btn" class="col-lg-1">
+        <!--<button id="add_board" class=""><i class="fas fa-plus"></i></button>-->
+      </div>
+      <div class="col-lg-7" style="padding: 10px;">
+        <div>
+          <!--<div class="col-lg-3">-->
+            <!--<label for="menu_title">메뉴명</label>-->
+            <!--<br>-->
+            <!--<label for="menu_explan">메뉴설명</label>-->
 
+          <!--</div>-->
+          <!--<div class="col-lg-8">-->
+            <!--<input type="text"id="menu_title" value="메뉴명" style="width: 100%; float: right;"><br>-->
+            <!--<input type="text"id="menu_explan" value="매뉴설명" style="width: 100%; float: right;"><br>-->
+
+          <!--</div>-->
+          <form>
+            <label for="menu_title">메뉴명</label>
+            <input class="mb_info" type="text"id="menu_title" value="메뉴명" style="width: 50%; float: right;"><br>
+            <label class="mb_info" for="menu_explan">메뉴설명</label>
+            <input type="text"id="menu_explan" value="매뉴설명" style="width: 50%; float: right;"><br>
+            <hr>
+            <label for="menu_autho">권한설정</label>
+            <select  id="menu_autho" style="width: 30%; float: right; margin-right:20%">
+              <option selected>선택</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+            </select>
+            <br>
+            <label for="com_auth">댓글 허용 <input type="checkbox" id="com_auth"checked /></label>
+            <br>
+            <label for="lik_aut">추천 허용 <input type="checkbox" id="lik_aut"checked /></label>
+
+          </form>
+          <div style="text-align: center">
+            <button>저장</button>
+
+          </div>
+          <!--<div class="row">-->
+            <!--<div class="col-4">-->
+              <!--<div class="list-group" id="list-tab" role="tablist">-->
+                <!--<a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>-->
+                <!--<a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>-->
+                <!--<a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>-->
+                <!--<a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>-->
+              <!--</div>-->
+            <!--</div>-->
+            <!--<div class="col-8">-->
+              <!--<div class="tab-content" id="nav-tabContent">-->
+                <!--<div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">-->
+                  <!--ㅁㄴㅇ-->
+                <!--</div>-->
+                <!--<div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>-->
+                <!--<div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>-->
+                <!--<div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>-->
+              <!--</div>-->
+            <!--</div>-->
+          <!--</div>-->
+          <div id="app-5">
+            <p>{{ message }}</p>
+            <button v-on:click="reverseMessage">메시지 뒤집기</button>
+          </div>
+
+        </div>
+
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "ManagementBoard",
-    }
+  export default {
+    name: "ManagementBoard",
+
+  }
+
 
 
 </script>
 
 <style scoped>
-  #createboard{
-    margin-bottom: 10px;
+  div{
+    /*border: 1px solid black;*/
   }
-  .custab{
-    /*border: 1px solid #ccc;*/
-    padding: 5px;
-    margin: 5% 0;
-    box-shadow: 3px 3px 2px #ccc;
-    transition: 0.5s;
+  #div_btn{
+    text-align: center;
   }
-  .custab:hover{
-    box-shadow: 3px 3px 0px transparent;
-    transition: 0.5s;
+  #add_board{
+    height: 150px;
+    width: 40px;
   }
-  a{
-    color: #000;
+  li{
+    font-size: 12px;
   }
-  .dropdown-toggle::after {
-    display:none;
+  .li_btn{
+    /*float: left;*/
   }
-
-
+  .color{
+    color: green;
+  }
+  div{
+    position: relative;
+  }
+  label{
+    margin-right: 10px;
+  }
+  input{
+    /*width: 50%; float: right;*/
+  }
 </style>
